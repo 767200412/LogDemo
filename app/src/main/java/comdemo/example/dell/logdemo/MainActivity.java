b047c05e048c0652ce7224f8d77f245ab453de99
767200412 <51736946+767200412@users.noreply.github.com>
2019-07-16 11:00:04 +0800

    实现手机号码按344空格格式

--- app/src/main/java/comdemo/example/dell/logdemo/MainActivity.java
+++ app/src/main/java/comdemo/example/dell/logdemo/MainActivity.java
@@ -2,6 +2,8 @@ package comdemo.example.dell.logdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +15,53 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mEditTextPhoneNumber = (EditText)findViewById(R.id.et_PhoneNumber);
+        //关键部分:自动分隔手机号码通过addTextChangedListener()实现
+        mEditTextPhoneNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s == null || s.length() == 0)
+                    return;
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < s.length(); i++) {
+                    if (i != 3 && i != 8 && s.charAt(i) == ' ') {
+                        continue;
+                    } else {
+                        stringBuilder.append(s.charAt(i));
+                        if ((stringBuilder.length() == 4 || stringBuilder.length() == 9)
+                                && stringBuilder.charAt(stringBuilder.length() - 1) != ' ') {
+                            stringBuilder.insert(stringBuilder.length() - 1, ' ');
+                        }
+                    }
+                }
+                if (!stringBuilder.toString().equals(s.toString())) {
+                    int index = start + 1;
+                    if (stringBuilder.charAt(start) == ' ') {
+                        if (before == 0) {
+                            index++;
+                        } else {
+                            index--;
+                        }
+                    } else {
+                        if (before == 1) {
+                            index--;
+                        }
+                    }
+                    mEditTextPhoneNumber.setText(stringBuilder.toString());
+                    mEditTextPhoneNumber.setSelection(index);
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
+
+
 }
